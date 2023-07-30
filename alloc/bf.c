98a0c031dbb6d24d0be09eca92d6f6c8f66644af
Harishankar Saju <88574754+Judgingboy@users.noreply.github.com>
2023-07-30 11:00:12 +0530

    Update bf.c

--- alloc/bf.c
+++ alloc/bf.c
@@ -1,49 +1,46 @@
 #include <stdio.h>
-int main()
+#include <conio.h>
+#define max 25
+void main()
 {
-   int block[50], //for storing the block size
-       data[50], // for storing the data size
-       nb,nd, // for storing the number of data and blocks respectivey 
-       temp,  // for storing the fragment temporarily 
-       b_order[50], // for storing the order of allocation of blocks
-       fragments[50],
-       allocated[50];// for storing the smallest block
-   printf("Enter the number of blocks: ");
-   scanf("%d",&nb);
-   printf("Enter the block Sizes: \n");
-   for(int i=0;i<nb;i++)
-   {
-     printf("Block %d: ",i+1);
-     scanf("%d",&block[i]);
-   }
-
-   int smallest = block[0];
-
-   printf("Enter the number of data: ");
-   scanf("%d",&nd);
-   printf("Enter the data Sizes: \n");
-   for(int i=0;i<nd;i++)
-   {
-     printf("Data %d: ",i+1);
-     scanf("%d",&data[i]);
-   }
-
-   for(int i=0;i<nd;i++)
-   {
-      for(int j=0;j<nb;j++)
-      {
-        if(allocated[i]!=1)
+    int frag[max], b[max], f[max], i, j, nb, nf, temp, lowest = 10000;
+    static int bf[max], ff[max];
+    printf("\nEnter the number of blocks:");
+    scanf("%d", &nb);
+    printf("Enter the number of files:");
+    scanf("%d", &nf);
+    printf("\nEnter the size of the blocks:-\n");
+    for (i = 1; i <= nb; i++)
+    {
+        printf("Block %d:", i);
+        scanf("%d", &b[i]);
+    }
+    printf("Enter the size of the files :-\n");
+    for (i = 1; i <= nf; i++)
+    {
+        printf("File %d:", i);
+        scanf("%d", &f[i]);
+    }
+    for (i = 1; i <= nf; i++)
+    {
+        for (j = 1; j <= nb; j++)
         {
-            int k=0;
-            while(block[k]<smallest&&k<=nb)
+            if (bf[j] != 1)
             {
-               smallest = block[k];
-               k++;
+                temp = b[j] - f[i];
+                if (temp >= 0)
+                    if (lowest > temp)
+                    {
+                        ff[i] = j;
+                        lowest = temp;
+                    }
             }
-            
-            
-
         }
-      }
-   }
+        frag[i] = lowest;
+        bf[ff[i]] = 1;
+        lowest = 10000;
+    }
+    printf("\nFile No\tFile Size \tBlock No\tBlock Size\tFragment");
+    for (i = 1; i <= nf && ff[i] != 0; i++)
+        printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d", i, f[i], ff[i], b[ff[i]], frag[i]);
 }
